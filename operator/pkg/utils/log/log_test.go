f5a5ac34e38557db1a2960154185ee75218489d9
jamy.shih <jamy.shih@prophetstor.com>
2018-11-21 16:09:42 +0800

    support for automated log rotation
    Signed-off-by: jamy.shih <jamy.shih@prophetstor.com>

--- operator/pkg/utils/log/log_test.go
+++ operator/pkg/utils/log/log_test.go
@@ -17,7 +17,6 @@ limitations under the License.
 package log
 
 import (
-	"errors"
 	"io/ioutil"
 	"log"
 	"os"
@@ -25,11 +24,73 @@ import (
 	"strconv"
 	"strings"
 	"testing"
+	"time"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"google.golang.org/grpc/grpclog"
 )
+
+const timePattern = "[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9][0-9][0-9][0-9]Z"
+
+type testDateEncoder struct {
+	zapcore.PrimitiveArrayEncoder
+	output string
+}
+
+func (tde *testDateEncoder) AppendString(s string) {
+	tde.output = s
+}
+
+func TestTimestampProperYear(t *testing.T) {
+	testEnc := &testDateEncoder{}
+	cases := []struct {
+		name  string
+		input time.Time
+		want  string
+	}{
+		{"1", time.Date(1, time.April, 1, 1, 1, 1, 1, time.UTC), "0001"},
+		{"1989", time.Date(1989, time.February, 1, 1, 1, 1, 1, time.UTC), "1989"},
+		{"2017", time.Date(2017, time.January, 1, 1, 1, 1, 1, time.UTC), "2017"},
+		{"2083", time.Date(2083, time.March, 1, 1, 1, 1, 1, time.UTC), "2083"},
+		{"2573", time.Date(2573, time.June, 1, 1, 1, 1, 1, time.UTC), "2573"},
+		{"9999", time.Date(9999, time.May, 1, 1, 1, 1, 1, time.UTC), "9999"},
+	}
+
+	for _, v := range cases {
+		t.Run(v.name, func(t *testing.T) {
+			formatDate(v.input, testEnc)
+			if !strings.HasPrefix(testEnc.output, v.want) {
+				t.Errorf("formatDate(%v) => %s, want year: %s", v.input, testEnc.output, v.want)
+			}
+		})
+	}
+}
+
+func TestTimestampProperMicros(t *testing.T) {
+	testEnc := &testDateEncoder{}
+	cases := []struct {
+		name  string
+		input time.Time
+		want  string
+	}{
+		{"1", time.Date(2017, time.April, 1, 1, 1, 1, 1000, time.UTC), "1"},
+		{"99", time.Date(1989, time.February, 1, 1, 1, 1, 99000, time.UTC), "99"},
+		{"999", time.Date(2017, time.January, 1, 1, 1, 1, 999000, time.UTC), "999"},
+		{"9999", time.Date(2083, time.March, 1, 1, 1, 1, 9999000, time.UTC), "9999"},
+		{"99999", time.Date(2083, time.March, 1, 1, 1, 1, 99999000, time.UTC), "99999"},
+		{"999999", time.Date(2083, time.March, 1, 1, 1, 1, 999999000, time.UTC), "999999"},
+	}
+
+	for _, v := range cases {
+		t.Run(v.name, func(t *testing.T) {
+			formatDate(v.input, testEnc)
+			if !strings.HasSuffix(testEnc.output, v.want+"Z") {
+				t.Errorf("formatDate(%v) => %s, want micros: %s", v.input, testEnc.output, v.want)
+			}
+		})
+	}
+}
 
 func TestBasic(t *testing.T) {
 	cases := []struct {
@@ -39,40 +100,44 @@ func TestBasic(t *testing.T) {
 		caller     bool
 		stackLevel zapcore.Level
 	}{
-		{func() { Debug("Hello") }, ".*\tdebug\tHello", false, false, None},
-		{func() { Debugf("Hello") }, ".*\tdebug\tHello", false, false, None},
-		{func() { Debugw("Hello") }, ".*\tdebug\tHello", false, false, None},
-		{func() { Debuga("Hello") }, ".*\tdebug\tHello", false, false, None},
+		{func() { Debug("Hello") }, timePattern + "\tdebug\tHello", false, false, None},
+		{func() { Debugf("Hello") }, timePattern + "\tdebug\tHello", false, false, None},
+		{func() { Debugw("Hello") }, timePattern + "\tdebug\tHello", false, false, None},
+		{func() { Debuga("Hello") }, timePattern + "\tdebug\tHello", false, false, None},
 
-		{func() { Info("Hello") }, ".*\tinfo\tHello", false, false, None},
-		{func() { Infof("Hello") }, ".*\tinfo\tHello", false, false, None},
-		{func() { Infow("Hello") }, ".*\tinfo\tHello", false, false, None},
-		{func() { Infoa("Hello") }, ".*\tinfo\tHello", false, false, None},
+		{func() { Info("Hello") }, timePattern + "\tinfo\tHello", false, false, None},
+		{func() { Infof("Hello") }, timePattern + "\tinfo\tHello", false, false, None},
+		{func() { Infow("Hello") }, timePattern + "\tinfo\tHello", false, false, None},
+		{func() { Infoa("Hello") }, timePattern + "\tinfo\tHello", false, false, None},
 
-		{func() { Warn("Hello") }, ".*\twarn\tHello", false, false, None},
-		{func() { Warnf("Hello") }, ".*\twarn\tHello", false, false, None},
-		{func() { Warnw("Hello") }, ".*\twarn\tHello", false, false, None},
-		{func() { Warna("Hello") }, ".*\twarn\tHello", false, false, None},
+		{func() { Warn("Hello") }, timePattern + "\twarn\tHello", false, false, None},
+		{func() { Warnf("Hello") }, timePattern + "\twarn\tHello", false, false, None},
+		{func() { Warnw("Hello") }, timePattern + "\twarn\tHello", false, false, None},
+		{func() { Warna("Hello") }, timePattern + "\twarn\tHello", false, false, None},
 
-		{func() { Error("Hello") }, ".*\terror\tHello", false, false, None},
-		{func() { Errorf("Hello") }, ".*\terror\tHello", false, false, None},
-		{func() { Errorw("Hello") }, ".*\terror\tHello", false, false, None},
-		{func() { Errora("Hello") }, ".*\terror\tHello", false, false, None},
+		{func() { Error("Hello") }, timePattern + "\terror\tHello", false, false, None},
+		{func() { Errorf("Hello") }, timePattern + "\terror\tHello", false, false, None},
+		{func() { Errorw("Hello") }, timePattern + "\terror\tHello", false, false, None},
+		{func() { Errora("Hello") }, timePattern + "\terror\tHello", false, false, None},
 
 		{func() {
 			l := With(zap.String("key", "value"))
 			l.Debug("Hello")
-		}, ".*\tdebug\tHello\t{\"key\": \"value\"}", false, false, None},
+		}, timePattern + "\tdebug\tHello\t{\"key\": \"value\"}", false, false, None},
 
-		{func() { Debug("Hello") }, ".*\tdebug\tlog/log_test.go:.*\tHello", false, true, None},
+		{func() { Debug("Hello") }, timePattern + "\tdebug\tlog/log_test.go:.*\tHello", false, true, None},
 
-		{func() { Debug("Hello") }, "{\"level\":\"debug\",\"time\":\".*T.*\",\"caller\":\"log/log_test.go:.*\",\"msg\":\"Hello\",\"stack\":\".*\"}",
+		{func() { Debug("Hello") }, "{\"level\":\"debug\",\"time\":\"" + timePattern + "\",\"caller\":\"log/log_test.go:.*\",\"msg\":\"Hello\"," +
+			"\"stack\":\".*\"}",
 			true, true, zapcore.DebugLevel},
-		{func() { Info("Hello") }, "{\"level\":\"info\",\"time\":\".*T.*\",\"caller\":\"log/log_test.go:.*\",\"msg\":\"Hello\",\"stack\":\".*\"}",
+		{func() { Info("Hello") }, "{\"level\":\"info\",\"time\":\"" + timePattern + "\",\"caller\":\"log/log_test.go:.*\",\"msg\":\"Hello\"," +
+			"\"stack\":\".*\"}",
 			true, true, zapcore.DebugLevel},
-		{func() { Warn("Hello") }, "{\"level\":\"warn\",\"time\":\".*T.*\",\"caller\":\"log/log_test.go:.*\",\"msg\":\"Hello\",\"stack\":\".*\"}",
+		{func() { Warn("Hello") }, "{\"level\":\"warn\",\"time\":\"" + timePattern + "\",\"caller\":\"log/log_test.go:.*\",\"msg\":\"Hello\"," +
+			"\"stack\":\".*\"}",
 			true, true, zapcore.DebugLevel},
-		{func() { Error("Hello") }, "{\"level\":\"error\",\"time\":\".*T.*\",\"caller\":\"log/log_test.go:.*\",\"msg\":\"Hello\",\"stack\":\".*\"}",
+		{func() { Error("Hello") }, "{\"level\":\"error\",\"time\":\"" + timePattern + "\",\"caller\":\"log/log_test.go:.*\",\"msg\":\"Hello\"," +
+			"\"stack\":\".*\"}",
 			true, true, zapcore.DebugLevel},
 	}
 
@@ -104,6 +169,10 @@ func TestBasic(t *testing.T) {
 		})
 	}
 
+	// sadly, only testing whether we crash or not...
+	l := With(zap.String("Key", "Value"))
+	l.Debug("Hello")
+	l.Sync()
 }
 
 func TestEnabled(t *testing.T) {
@@ -154,20 +223,89 @@ func TestOddballs(t *testing.T) {
 	o.outputLevel = "foobar"
 	err := Configure(o)
 	if err == nil {
-		t.Errorf("Got success, expected failure")
+		t.Error("Got success, expected failure")
 	}
 
 	o = NewOptions()
 	o.stackTraceLevel = "foobar"
 	err = Configure(o)
 	if err == nil {
-		t.Errorf("Got success, expected failure")
+		t.Error("Got success, expected failure")
 	}
 
 	o = NewOptions()
-	err = configure(o, func(c *zap.Config) (*zap.Logger, error) { return nil, errors.New("BAD") })
+	o.OutputPaths = []string{"/JUNK"}
+	err = Configure(o)
 	if err == nil {
 		t.Errorf("Got success, expecting error")
+	}
+
+	o = NewOptions()
+	o.ErrorOutputPaths = []string{"/JUNK"}
+	err = Configure(o)
+	if err == nil {
+		t.Errorf("Got success, expecting error")
+	}
+}
+
+func TestRotateNoStdout(t *testing.T) {
+	// Ensure that rotation is setup properly
+
+	dir, _ := ioutil.TempDir("", "TestRotateNoStdout")
+	defer os.RemoveAll(dir)
+
+	file := dir + "/rot.log"
+
+	o := NewOptions()
+	o.OutputPaths = []string{}
+	o.RotateOutputPath = file
+	if err := Configure(o); err != nil {
+		t.Fatalf("Unable to configure logging: %v", err)
+	}
+
+	Error("HELLO")
+	Sync()
+
+	content, err := ioutil.ReadFile(file)
+	if err != nil {
+		t.Errorf("Got failure '%v', expecting success", err)
+	}
+
+	lines := strings.Split(string(content), "\n")
+	if !strings.Contains(lines[0], "HELLO") {
+		t.Errorf("Expecting for first line of log to contain HELLO, got %s", lines[0])
+	}
+}
+
+func TestRotateAndStdout(t *testing.T) {
+	dir, _ := ioutil.TempDir("", "TestRotateAndStdout")
+	defer os.RemoveAll(dir)
+
+	file := dir + "/rot.log"
+
+	stdoutLines, _ := captureStdout(func() {
+		o := NewOptions()
+		o.RotateOutputPath = file
+		if err := Configure(o); err != nil {
+			t.Fatalf("Unable to configure logger: %v", err)
+		}
+
+		Error("HELLO")
+		Sync()
+
+		content, err := ioutil.ReadFile(file)
+		if err != nil {
+			t.Errorf("Got failure '%v', expecting success", err)
+		}
+
+		rotLines := strings.Split(string(content), "\n")
+		if !strings.Contains(rotLines[0], "HELLO") {
+			t.Errorf("Expecting for first line of log to contain HELLO, got %s", rotLines[0])
+		}
+	})
+
+	if !strings.Contains(stdoutLines[0], "HELLO") {
+		t.Errorf("Expecting for first line of log to contain HELLO, got %s", stdoutLines[0])
 	}
 }
 
@@ -188,9 +326,9 @@ func TestCapture(t *testing.T) {
 	})
 
 	patterns := []string{
-		".*\tinfo\tlog/log_test.go:.*\tHello",
-		".*\tinfo\tlog/log_test.go:.*\tThere",
-		".*\tinfo\tlog/log_test.go:.*\tGoodbye",
+		timePattern + "\tinfo\tlog/log_test.go:.*\tHello",
+		timePattern + "\tinfo\tlog/log_test.go:.*\tThere",
+		timePattern + "\tinfo\tlog/log_test.go:.*\tGoodbye",
 	}
 
 	for i, pat := range patterns {
