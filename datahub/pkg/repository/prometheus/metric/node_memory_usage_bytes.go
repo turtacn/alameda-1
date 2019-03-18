39149ccc0f068ed8ae5b80765b7302018af3d1af
bob.huang <bob.huang@prophetstor.com>
2019-03-18 12:12:23 +0800

    modify query expression of metrics nodeMemoryUsageByte
    Signed-off-by: bob.huang <bob.huang@prophetstor.com>

--- datahub/pkg/repository/prometheus/metric/node_memory_usage_bytes.go
+++ datahub/pkg/repository/prometheus/metric/node_memory_usage_bytes.go
@@ -1,6 +1,9 @@
 package metric
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/containers-ai/alameda/datahub/pkg/entity/prometheus/nodeMemoryBytesTotal"
 	"github.com/containers-ai/alameda/datahub/pkg/entity/prometheus/nodeMemoryUtilization"
 	"github.com/containers-ai/alameda/datahub/pkg/repository/prometheus"
@@ -26,88 +29,97 @@ func NewNodeMemoryUsageBytesRepositoryWithConfig(cfg prometheus.Config) NodeMemo
 func (n NodeMemoryUsageBytesRepository) ListMetricsByNodeName(nodeName string, options ...Option) ([]prometheus.Entity, error) {
 
 	var (
-		nodeMemoryUsageBytesEntities = make([]prometheus.Entity, 0)
+		err error
+
+		prometheusClient *prometheus.Prometheus
+
+		nodeMemoryBytesTotalQueryExpression    string
+		nodeMemoryBytesTotalMetricName         string
+		nodeMemoryBytesTotalQueryLabelsString  string
+		nodeMemoryUtilizationQueryExpression   string
+		nodeMemoryUtilizationMetricName        string
+		nodeMemoryUtilizationQueryLabelsString string
+		queryExpression                        string
+
+		response prometheus.Response
+
+		entities []prometheus.Entity
 	)
 
-	nodeMemoryBytesTotalRepository := NewNodeMemoryBytesTotalRepositoryWithConfig(n.PrometheusConfig)
-	nodeMemoryUtilizationRepository := NewNodeMemoryUtilizationRepositoryWithConfig(n.PrometheusConfig)
-
-	errChan := make(chan error)
-	entitiesChan := make(chan []prometheus.Entity)
-	fetchFunctions := []nodeMetricsFetchingFunction{nodeMemoryBytesTotalRepository.ListMetricsByNodeName, nodeMemoryUtilizationRepository.ListMetricsByNodeName}
-	entitiesSlice := make([][]prometheus.Entity, len(fetchFunctions))
-	for _, f := range fetchFunctions {
-		go n.fetchNodeMetricsFromFunction(f, entitiesChan, errChan, nodeName, options...)
-	}
-
-	goroutineDoneIndex := 0
-	for goroutineDoneIndex < len(fetchFunctions) {
-		select {
-		case entities := <-entitiesChan:
-			entitiesSlice[goroutineDoneIndex] = entities
-		case err := <-errChan:
-			return nodeMemoryUsageBytesEntities, errors.Wrap(err, "list node memory usage bytes by node name failed")
-		}
-		goroutineDoneIndex++
-	}
-
-	nodeMemoryUsageBytesEntities, err := n.buildEntitiesFromNodeMemoryBytesTotalEntitesMultiplyNodeMemoryUtilizationEntities(entitiesSlice[0], entitiesSlice[1])
+	prometheusClient, err = prometheus.New(n.PrometheusConfig)
 	if err != nil {
-		return nodeMemoryUsageBytesEntities, errors.Wrap(err, "list node memory usage bytes by node name failed")
+		return entities, errors.Wrap(err, "list node memory usage by node name failed")
 	}
 
-	return nodeMemoryUsageBytesEntities, nil
-}
+	opt := buildDefaultOptions()
+	for _, option := range options {
+		option(&opt)
+	}
+	stepTimeInSeconds := int64(opt.stepTime.Nanoseconds() / int64(time.Second))
 
-func (n NodeMemoryUsageBytesRepository) fetchNodeMetricsFromFunction(fetchFunction nodeMetricsFetchingFunction, entitiesChan chan []prometheus.Entity, errorChan chan error, nodeName string, options ...Option) {
-	entities, err := fetchFunction(nodeName, options...)
-	if err != nil {
-		errorChan <- err
+	nodeMemoryBytesTotalMetricName = nodeMemoryBytesTotal.MetricName
+	nodeMemoryBytesTotalQueryLabelsString = n.buildNodeMemoryBytesTotalQueryLabelsStringByNodeName(nodeName)
+	if nodeMemoryBytesTotalQueryLabelsString != "" {
+		nodeMemoryBytesTotalQueryExpression = fmt.Sprintf("%s{%s}", nodeMemoryBytesTotalMetricName, nodeMemoryBytesTotalQueryLabelsString)
 	} else {
-		entitiesChan <- entities
+		nodeMemoryBytesTotalQueryExpression = fmt.Sprintf("%s", nodeMemoryBytesTotalMetricName)
 	}
+	nodeMemoryBytesTotalQueryExpression, err = wrapQueryExpressionWithAggregationOverTimeFunction(nodeMemoryBytesTotalQueryExpression, opt.aggregateOverTimeFunc, stepTimeInSeconds)
+	if err != nil {
+		return entities, errors.Wrap(err, "list node memory usage metrics by node name failed")
+	}
+
+	nodeMemoryUtilizationMetricName = nodeMemoryUtilization.MetricName
+	nodeMemoryUtilizationQueryLabelsString = n.buildNodeMemoryUtilizationQueryLabelsStringByNodeName(nodeName)
+	if nodeMemoryUtilizationQueryLabelsString != "" {
+		nodeMemoryUtilizationQueryExpression = fmt.Sprintf("%s{%s}", nodeMemoryUtilizationMetricName, nodeMemoryUtilizationQueryLabelsString)
+	} else {
+		nodeMemoryUtilizationQueryExpression = fmt.Sprintf("%s", nodeMemoryUtilizationMetricName)
+	}
+	nodeMemoryUtilizationQueryExpression, err = wrapQueryExpressionWithAggregationOverTimeFunction(nodeMemoryUtilizationQueryExpression, opt.aggregateOverTimeFunc, stepTimeInSeconds)
+	if err != nil {
+		return entities, errors.Wrap(err, "list node memory usage metrics by node name failed")
+	}
+
+	queryExpression = fmt.Sprintf("%s * %s", nodeMemoryBytesTotalQueryExpression, nodeMemoryUtilizationQueryExpression)
+
+	response, err = prometheusClient.QueryRange(queryExpression, opt.startTime, opt.endTime, opt.stepTime)
+	if err != nil {
+		return entities, errors.Wrap(err, "list node memory bytes total by node name failed")
+	} else if response.Status != prometheus.StatusSuccess {
+		return entities, errors.Errorf("list node memory bytes total by node name failed: receive error response from prometheus: %s", response.Error)
+	}
+
+	entities, err = response.GetEntitis()
+	if err != nil {
+		return entities, errors.Wrap(err, "list node memory usage by node name failed")
+	}
+
+	return entities, nil
 }
 
-func (n NodeMemoryUsageBytesRepository) buildEntitiesFromNodeMemoryBytesTotalEntitesMultiplyNodeMemoryUtilizationEntities(firstEntites, secondEntities []prometheus.Entity) ([]prometheus.Entity, error) {
+func (n NodeMemoryUsageBytesRepository) buildNodeMemoryBytesTotalQueryLabelsStringByNodeName(nodeName string) string {
 
 	var (
-		nodeMemoryUsageBytesEntities = make([]prometheus.Entity, 0)
-		nodeMemoryBytesTotaEntityMap = make(map[string]prometheus.Entity)
+		queryLabelsString = ""
 	)
 
-	if len(firstEntites) != len(secondEntities) {
-		return nodeMemoryUsageBytesEntities, errors.Errorf("build entities failed: length of nodeMemoryBytesTotal entities is not equal to length of nodeMemoryUtilization entities")
+	if nodeName != "" {
+		queryLabelsString += fmt.Sprintf(`%s = "%s"`, nodeMemoryBytesTotal.NodeLabel, nodeName)
 	}
 
-	for _, entity := range firstEntites {
-		if nodeName, exist := entity.Labels[nodeMemoryBytesTotal.NodeLabel]; !exist {
-			return nodeMemoryUsageBytesEntities, errors.Errorf("build entities failed: node label %s doe not exist in entity's labels,received entity: %+v", nodeMemoryBytesTotal.NodeLabel, entity)
-		} else {
-			nodeMemoryBytesTotaEntityMap[nodeName] = entity
-		}
+	return queryLabelsString
+}
+
+func (n NodeMemoryUsageBytesRepository) buildNodeMemoryUtilizationQueryLabelsStringByNodeName(nodeName string) string {
+
+	var (
+		queryLabelsString = ""
+	)
+
+	if nodeName != "" {
+		queryLabelsString += fmt.Sprintf(`%s = "%s"`, nodeMemoryUtilization.NodeLabel, nodeName)
 	}
 
-	for _, entity := range secondEntities {
-		nodeName, exist := entity.Labels[nodeMemoryUtilization.NodeLabel]
-		if !exist {
-			return nodeMemoryUsageBytesEntities, errors.Errorf("build entities failed: node label %s doe not exist in entity's labels,received entity: %+v", nodeMemoryBytesTotal.NodeLabel, entity)
-		}
-		firstEntity, exist := nodeMemoryBytesTotaEntityMap[nodeName]
-		if !exist {
-			return nodeMemoryUsageBytesEntities, errors.Errorf("build entities failed: node name %s does not present in both nodeMemoryUtilization and nodeMemoryBytesTotal entities", nodeName)
-		}
-
-		options := []operateOption{
-			operateOptionsMatchType(matchTypeOn),
-			operateOptionsLabels([]string{nodeMemoryUtilization.NodeLabel}),
-		}
-		usageBytesEntity, err := oneToOneMultiply(firstEntity, entity, options...)
-		if err != nil {
-			return nodeMemoryUsageBytesEntities, errors.Wrap(err, "build entities failed:")
-		}
-		nodeMemoryUsageBytesEntities = append(nodeMemoryUsageBytesEntities, usageBytesEntity)
-	}
-
-	return nodeMemoryUsageBytesEntities, nil
-
+	return queryLabelsString
 }
