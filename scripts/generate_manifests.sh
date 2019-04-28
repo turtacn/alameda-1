c957262c370d20fc800f13f8f95aa7df0fb237e6
Matt Wu <matt.wu@prophetstor.com>
2019-04-28 22:27:05 +0800

    fix fail to install alameda with helm chart
    Signed-off-by: Matt Wu <matt.wu@prophetstor.com>

--- scripts/generate_manifests.sh
+++ scripts/generate_manifests.sh
@@ -14,10 +14,16 @@ fi
 chartdir=$1
 values=$2
 
+NAMESPACE=alameda
 tmpdir=`mktemp -d 2>/dev/null || mktemp -d -t 'charttmpdir'`
 tmpchartdir=${tmpdir}
 tmpoutputdir=${tmpdir}/output
-
+namespaceResource="
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: $NAMESPACE
+"
 #cp -R helm/alameda ${tmpchartdir}
 #echo "Version: $1" >> ${charttmpdir}/Chart.yaml
 
@@ -25,27 +31,35 @@ mkdir -p ${chartdir}/alameda-operator/ ${chartdir}/admission-controller/ ${chart
 
 # generate Alameda manifests
 mkdir -p ${tmpoutputdir}
-helm template --name alameda --namespace alameda -f ${values} helm/alameda --output-dir ${tmpoutputdir}
+helm template --name alameda --namespace $NAMESPACE -f ${values} helm/alameda --output-dir ${tmpoutputdir}
 
 mv ${tmpoutputdir}/alameda/templates/* ${chartdir}/alameda-operator/
+echo "$namespaceResource" > ${chartdir}/alameda-operator/namespace.yaml
 mv ${tmpoutputdir}/alameda/charts/admission-controller/templates/* ${chartdir}/admission-controller/
+echo "$namespaceResource" > ${chartdir}/admission-controller/namespace.yaml
 mv ${tmpoutputdir}/alameda/charts/alameda-ai/templates/* ${chartdir}/alameda-ai/
+echo "$namespaceResource" > ${chartdir}/alameda-ai/namespace.yaml
 mv ${tmpoutputdir}/alameda/charts/datahub/templates/* ${chartdir}/alameda-datahub/
+echo "$namespaceResource" > ${chartdir}/alameda-datahub/namespace.yaml
 mv ${tmpoutputdir}/alameda/charts/evictioner/templates/* ${chartdir}/alameda-evictioner/
+echo "$namespaceResource" > ${chartdir}/alameda-evictioner/namespace.yaml
 
 # generate InfluxDB manifests
 mkdir -p ${tmpoutputdir}
 helm fetch stable/influxdb --version 1.1.3 --untar --untardir ${tmpchartdir}
-helm template --name alameda-influxdb --namespace alameda ${tmpchartdir}/influxdb --output-dir ${tmpoutputdir}
+helm template --name alameda-influxdb --namespace $NAMESPACE ${tmpchartdir}/influxdb --output-dir ${tmpoutputdir}
 
 mkdir -p ${chartdir}/alameda-influxdb/
 mv ${tmpoutputdir}/influxdb/templates/* ${chartdir}/alameda-influxdb/
+echo "$namespaceResource" > ${chartdir}/alameda-influxdb/namespace.yaml
 
 # generate grafana manifests
 mkdir -p ${tmpoutputdir}
-helm template --name alameda-grafana --namespace alameda ./helm/grafana/ --output-dir ${tmpoutputdir}
+helm template --name alameda-grafana --namespace $NAMESPACE ./helm/grafana/ --output-dir ${tmpoutputdir}
 
 mkdir -p ${chartdir}/alameda-grafana/
 mv ${tmpoutputdir}/grafana/templates/* ${chartdir}/alameda-grafana/
+echo "$namespaceResource" > ${chartdir}/alameda-grafana/namespace.yaml
 
 rm -rf ${tmpdir}
+
