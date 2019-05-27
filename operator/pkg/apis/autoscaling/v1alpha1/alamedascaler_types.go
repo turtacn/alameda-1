0171c8d5edc784f7bce55e3b8b689698da3eb73b
Frederic Huang <kuofu.huang@prophetstor.com>
2019-05-27 14:20:34 +0800

    remove required properties from status object of alamedascaler crd (#471)
    * remove required properties from status object of alamedascaler crd
    
    * rollback camel case change
    
    Signed-off-by: Frederic Huang <kuofu.huang@prophetstor.com>

--- operator/pkg/apis/autoscaling/v1alpha1/alamedascaler_types.go
+++ operator/pkg/apis/autoscaling/v1alpha1/alamedascaler_types.go
@@ -61,7 +61,7 @@ type AlamedaResource struct {
 
 type AlamedaController struct {
 	Deployments       map[NamespacedName]AlamedaResource `json:"deployments" protobuf:"bytes,1,opt,name=deployments"`
-	DeploymentConfigs map[NamespacedName]AlamedaResource `json:"deploymentConfigs" protobuf:"bytes,2,opt,name=deployment_configs"`
+	DeploymentConfigs map[NamespacedName]AlamedaResource `json:"deploymentconfigs" protobuf:"bytes,2,opt,name=deployment_configs"`
 }
 
 type AlamedaControllerType int
@@ -83,7 +83,7 @@ var (
 type AlamedaScalerSpec struct {
 	// Important: Run "make" to regenerate code after modifying this file
 	Selector        *metav1.LabelSelector `json:"selector" protobuf:"bytes,1,name=selector"`
-	EnableExecution enableExecution       `json:"enableExecution" protobuf:"bytes,2,name=enable_execution"`
+	EnableExecution enableExecution       `json:"enableexecution" protobuf:"bytes,2,name=enable_execution"`
 	// +kubebuilder:validation:Enum=stable,compact
 	Policy                alamedaPolicy `json:"policy,omitempty" protobuf:"bytes,3,opt,name=policy"`
 	CustomResourceVersion string        `json:"customResourceVersion,omitempty" protobuf:"bytes,4,opt,name=custom_resource_version"`
