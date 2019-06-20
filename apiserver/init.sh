043d07b79ec3370ec228bd195245d43e4d7d960a
CheeSiong.Lee <CheeSiong.Lee@ProphetStor.com>
2019-06-20 10:59:40 +0800

    [container] apiserver,datapipe - add logrotate

--- apiserver/init.sh
+++ apiserver/init.sh
@@ -2,9 +2,32 @@
 #
 [ "${AIHOME}" = "" ] && export AIHOME=/opt/alameda/apiserver
 
-#
-while :
-do
+# create pwent entry because logrotate need the running uid inside /etc/passwd
+MY_UID="`id -u`"
+if [ "`cat /etc/passwd | awk -F: '{print $3}' | grep \"^${MY_UID}$\"`" = "" ]; then
+    sed -e "s/alameda:/alameda2:/g" /etc/passwd > /tmp/.tmpfn; cat /tmp/.tmpfn > /etc/passwd; rm -f /tmp/.tmpfn
+    echo "alameda:x:${MY_UID}:0:Federator.ai:${AIHOME}:/bin/sh" >> /etc/passwd
+fi
+
+cron_run_hourly()
+{
+    sleep_time=3600
+    while :; do
+        case "`date +%H`" in
+            "00") # hour is 00
+                logrotate -v -f /etc/logrotate.conf
+                ;;
+        esac
+        sleep ${sleep_time}
+    done
+    exit 0
+}
+
+# start crond
+cron_run_hourly &
+
+# start main service
+while :; do
     cd ${AIHOME}/bin
     ${AIHOME}/bin/apiserver run
     [ -f /tmp/.pause ] && sleep 300 || sleep 30
