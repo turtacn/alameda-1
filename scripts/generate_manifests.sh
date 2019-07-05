c194e1329cdc9d105641121ebc55822cafc467fc
Frederic Huang <kuofu.huang@prophetstor.com>
2019-07-05 23:13:26 +0800

    add recommender, executor, ai-dispatcher, and rabbitmq components in helm
    Signed-off-by: Frederic Huang <kuofu.huang@prophetstor.com>
    (cherry picked from commit 8505585a103737cbd70981acb2808938188453d0)

--- scripts/generate_manifests.sh
+++ scripts/generate_manifests.sh
@@ -1,18 +1,43 @@
 #!/usr/bin/env bash
 
 # requires helm to be installed
+cd `pwd`/$(dirname $BASH_SOURCE)/..
+REPO_ROOT_DIR=`pwd`
+chartdir=$REPO_ROOT_DIR/example/deployment/kubernetes/
+values=$REPO_ROOT_DIR/helm/alameda/values.yaml
 
-if [[ ${#@} < 2 ]]; then
-    echo "Usage: $0 chart values"
-#    echo "* semver: semver-formatted version for this package"
-    echo "* chart: the directory to output the chart"
-    echo "* values: the values file"
-    exit 1
-fi
-
-#version=$1
-chartdir=$1
-values=$2
+while test $# -gt 0;do
+    case "$1" in
+        -h|--help)
+            echo "-f, --values=VALUES     specify values file to use"
+            echo "-o, --output-dir=DIR    specify a folder to output result"
+            exit 0
+            ;;
+	-f|--values)
+            shift
+            if test $# -gt 0;then
+                values=$1
+            else
+                echo "please specify the correct values file"
+                exit 1
+            fi
+            shift
+            ;;
+        -o|--output-dir)
+            shift
+            if test $# -gt 0;then
+                chartdir=$1
+            else
+                echo "please specify the correct output folder"
+                exit 1
+            fi
+            shift
+            ;;
+	*)
+            break
+            ;;
+    esac
+done
 
 NAMESPACE=alameda
 tmpdir=`mktemp -d 2>/dev/null || mktemp -d -t 'charttmpdir'`
@@ -27,11 +52,16 @@ metadata:
 #cp -R helm/alameda ${tmpchartdir}
 #echo "Version: $1" >> ${charttmpdir}/Chart.yaml
 
-mkdir -p ${chartdir}/alameda-operator/ ${chartdir}/admission-controller/ ${chartdir}/alameda-ai/ ${chartdir}/alameda-datahub/ ${chartdir}/alameda-evictioner/
+mkdir -p ${chartdir}/alameda-operator/ ${chartdir}/admission-controller/ \
+    ${chartdir}/alameda-ai/ ${chartdir}/alameda-datahub/ ${chartdir}/alameda-evictioner/ \
+    ${chartdir}/alameda-recommender/ ${chartdir}/alameda-executor/ \
+    ${chartdir}/alameda-ai-dispatcher/
+# isolate rabbitmq component due to it is controlled by predictQueueEnable flag
+mkdir -p ${chartdir}/alameda-rabbitmq/
 
 # generate Alameda manifests
 mkdir -p ${tmpoutputdir}
-helm template --name alameda --namespace $NAMESPACE -f ${values} helm/alameda --output-dir ${tmpoutputdir}
+helm template --name alameda --namespace $NAMESPACE -f ${values} $REPO_ROOT_DIR/helm/alameda --output-dir ${tmpoutputdir}
 
 mv ${tmpoutputdir}/alameda/templates/* ${chartdir}/alameda-operator/
 echo "$namespaceResource" > ${chartdir}/alameda-operator/namespace.yaml
@@ -43,11 +73,21 @@ mv ${tmpoutputdir}/alameda/charts/datahub/templates/* ${chartdir}/alameda-datahu
 echo "$namespaceResource" > ${chartdir}/alameda-datahub/namespace.yaml
 mv ${tmpoutputdir}/alameda/charts/evictioner/templates/* ${chartdir}/alameda-evictioner/
 echo "$namespaceResource" > ${chartdir}/alameda-evictioner/namespace.yaml
+mv ${tmpoutputdir}/alameda/charts/recommender/templates/* ${chartdir}/alameda-recommender/
+echo "$namespaceResource" > ${chartdir}/alameda-recommender/namespace.yaml
+mv ${tmpoutputdir}/alameda/charts/executor/templates/* ${chartdir}/alameda-executor/
+echo "$namespaceResource" > ${chartdir}/alameda-executor/namespace.yaml
+
+# predictQueueEnable is false, no files can be moved in ai-dispatcher templates
+mv ${tmpoutputdir}/alameda/charts/ai-dispatcher/templates/* ${chartdir}/alameda-ai-dispatcher/
+echo "$namespaceResource" > ${chartdir}/alameda-ai-dispatcher/namespace.yaml
+mv ${tmpoutputdir}/alameda/charts/rabbitmq/templates/* ${chartdir}/alameda-rabbitmq/
+echo "$namespaceResource" > ${chartdir}/alameda-rabbitmq/namespace.yaml
 
 # generate InfluxDB manifests
 mkdir -p ${tmpoutputdir}
-helm fetch stable/influxdb --version 1.1.3 --untar --untardir ${tmpchartdir}
-helm template --name alameda-influxdb --namespace $NAMESPACE ${tmpchartdir}/influxdb --output-dir ${tmpoutputdir}
+helm fetch stable/influxdb --version 1.1.9 --untar --untardir ${tmpchartdir}
+helm template --name alameda-influxdb --namespace $NAMESPACE ${tmpchartdir}/influxdb --set persistence.enabled=false --output-dir ${tmpoutputdir}
 
 mkdir -p ${chartdir}/alameda-influxdb/
 mv ${tmpoutputdir}/influxdb/templates/* ${chartdir}/alameda-influxdb/
@@ -55,11 +95,19 @@ echo "$namespaceResource" > ${chartdir}/alameda-influxdb/namespace.yaml
 
 # generate grafana manifests
 mkdir -p ${tmpoutputdir}
-helm template --name alameda-grafana --namespace $NAMESPACE ./helm/grafana/ --output-dir ${tmpoutputdir}
+helm template --name alameda-grafana --namespace $NAMESPACE $REPO_ROOT_DIR/helm/grafana/ --output-dir ${tmpoutputdir}
 
 mkdir -p ${chartdir}/alameda-grafana/
 mv ${tmpoutputdir}/grafana/templates/* ${chartdir}/alameda-grafana/
 echo "$namespaceResource" > ${chartdir}/alameda-grafana/namespace.yaml
+
+# generate rabbitmq manifests
+#mkdir -p ${tmpoutputdir}
+#helm template --name alameda-rabbitmq --namespace $NAMESPACE $REPO_ROOT_DIR/helm/alameda/charts/rabbitmq/ --output-dir ${tmpoutputdir}
+
+#mkdir -p ${chartdir}/alameda-rabbitmq/
+#mv ${tmpoutputdir}/rabbitmq/templates/* ${chartdir}/alameda-rabbitmq/
+#echo "$namespaceResource" > ${chartdir}/alameda-rabbitmq/namespace.yaml
 
 rm -rf ${tmpdir}
 
