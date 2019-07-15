c1616112a6ee7c9267006a99ecc778b6e177f442
kit.lin <kit.lin@prophetstor.com>
2019-07-15 11:09:00 +0800

    [alameda] Add types and utils in internal prometheus library.
    Signed-off-by: kit.lin <kit.lin@prophetstor.com>

--- internal/pkg/database/prometheus/types.go
+++ internal/pkg/database/prometheus/types.go
@@ -1,1 +1,7 @@
 package prometheus
+
+import (
+	DBCommon "github.com/containers-ai/alameda/internal/pkg/database/common"
+)
+
+type nodeMetricsFetchingFunction func(nodeName string, options ...DBCommon.Option) ([]Entity, error)
