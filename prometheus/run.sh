df43575378f0de5fb1a3dd9382412af34ca0f389
tuxudong <I1>
2021-03-23 15:24:23 +0800

    turta/alameda dev branch merged v1 haproxy & deploy prometheus service

---
+++ prometheus/run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# install helm 3
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+# add & download repo
+helm repo add azure	http://mirror.azure.cn/kubernetes/charts/
+helm pull azure/prometheus-operator
+# unarchived & install to specific namespace
+tar zxvf prometheus-operator-9.3.2.tgz
+cd prometheus-operator
+kubectl create ns monitoring
+kubectl apply -f crds/
+helm install prometheus -n monitoring .
