ed332db526dff9fb896089e170f3b13662b1f0b0
tuxudong <I1>
2021-03-26 18:28:48 +0800

    optimizaed prometheus deployment

--- prometheus/run.sh
+++ prometheus/run.sh
@@ -3,10 +3,10 @@
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
-# add & download repo
+
+## 选择一，已经过期了
 helm repo add azure http://mirror.azure.cn/kubernetes/charts/
 helm pull azure/prometheus-operator
-# unarchived & install to specific namespace
 tar zxvf prometheus-operator-9.3.2.tgz
 cd prometheus-operator
 kubectl create ns monitoring
